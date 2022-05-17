d1d7979dcd018e0efde0630f3ed150896c7660a3
brnftcbrr <bruno.ftcbrr@gmail.com>
2022-05-16 21:48:31 -0300

    Comitado

---
+++ js/firebase.js
@@ -0,0 +1,20 @@
+// Your web app's Firebase configuration
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCvtt98wN8gNqPYTFr8n5bKUY_pnoY_tEM",
+
+  authDomain: "appteste-e1f2d.firebaseapp.com",
+
+  databaseURL: "https://appteste-e1f2d-default-rtdb.firebaseio.com",
+
+  projectId: "appteste-e1f2d",
+
+  storageBucket: "appteste-e1f2d.appspot.com",
+
+  messagingSenderId: "1048255507910",
+
+  appId: "1:1048255507910:web:78519418e1152706e0d6c7"
+
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
